684a5c896e7861f283336990266716812df081de
Iasser99 <iasserchahaud@gmail.com>
2025-09-01 19:36:33 -0300

    subir tp 8

---
+++ TP JS/JS-002_A.js
@@ -0,0 +1,6 @@
+document.write("Hola mundo");
+document.write("<hr>");
+document.write("<h1>inciso C del punto 2</h1>");
+document.write("script en head: Es para cosas que deben estar listas antes de que se muestre la página.<br>");
+document.write("script en body: Es para cosas rápidas que afectan al contenido visible<br>");
+document.write("script externo: Para proyectos grandes y reutilizables");
